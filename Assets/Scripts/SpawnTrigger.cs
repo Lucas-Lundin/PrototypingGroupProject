1744e21856717b9c6d6eeb87898ccbce900643ec
Amos Johan Persson <amos.persson@edu.futuregames.nu>
2022-10-06 17:17:41 +0200

    support for non-repeatable spawn triggers

--- Assets/Scripts/SpawnTrigger.cs
+++ Assets/Scripts/SpawnTrigger.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class SpawnTrigger : MonoBehaviour
 {
     [SerializeField] private Spawner[] spawners;
+    [SerializeField] private bool repeatable;
+    private bool active;
+
+    private void Start()
+    {
+        active = true;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && active)
         {
             foreach (Spawner spawnPoint in spawners)
             {
                 EnemyManager.SpawnByObject(spawnPoint);
+            }
+            if (!repeatable)
+            {
+                active = false;
             }
         }
     }
