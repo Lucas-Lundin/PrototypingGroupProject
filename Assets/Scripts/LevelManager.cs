50abb0ac200c0ad7a172ed55107e3e9c0fc55c36
Amos Johan Persson <amos.persson@edu.futuregames.nu>
2022-10-06 11:44:26 +0200

    fix level switching "out of bounds" bug

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -24,7 +24,7 @@ public class LevelManager : MonoBehaviour
     public static void LoadNext()
     {
         var LM = GetInstance();
-        if (LM.currentLevel < SceneManager.sceneCount)
+        if (LM.currentLevel + 1 < SceneManager.sceneCount)
         {
             SceneManager.LoadSceneAsync(LM.currentLevel + 1);
         }
